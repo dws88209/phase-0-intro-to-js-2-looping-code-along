de13a1f522deb6c71f7ab48bbccd420ab756f463
Destinee Saunders <dsaunders290@gmail.com>
2022-07-31 10:50:17 -0400

    commit tree ee28d96f70bafb3ba613cfff34299f9c5f78e32f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+
 // Code your solutions in this file
+ const Cards = []
+
+ function writeCards(Cards, event){
+    const newarray = []
+    for(let name = 0; name < Cards.length; name++) {
+         newarray.push(`Thank you, ${Cards[name]}, for the wonderful ${event} gift!`);
+         debugger;
+    }
+    return newarray;
+ }
+ writeCards(Cards);
+
+ function countDown (i){
+    while(i >= 0) {
+    console.log(i);
+    i--;
+    }
+}
+ 
+
